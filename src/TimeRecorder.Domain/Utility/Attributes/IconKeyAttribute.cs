e866047a1ae989bb2bedb603d59a7a1e4fa15669
ambleside138 <ambleside.138@gmail.com>
2020-05-09 19:16:07 +0900

    feat: update contents
    ・スクロールバーのスタイル変更
    ・時系列Canvasの追加
    ・アイコンコンバータ実装

---
+++ src/TimeRecorder.Domain/Utility/Attributes/IconKeyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeRecorder.Domain.Utility.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class IconKeyAttribute : Attribute
+    {
+        public IconKeyAttribute(string key)
+        {
+            Key = key;
+        }
+
+        /// <summary>
+        /// 表示名称
+        /// </summary>
+        public string Key { get; }
+    }
+}
