8a37cffc85c5eee50a9117fdb5ad38b6dc8e172f
ambleside138 <ambleside.138@gmail.com>
2020-05-16 17:23:32 +0900

    feat:add change theme func
    色一覧の表示まで

--- src/TimeRecorder/Contents/Configuration/ConfigurationViewModel.cs
+++ src/TimeRecorder/Contents/Configuration/ConfigurationViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Livet;
+using MaterialDesignColors;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TimeRecorder.Contents;
 using TimeRecorder.NavigationRail.ViewModels;
@@ -12,9 +14,17 @@ namespace TimeRecorder.Contents.Configuration
     {
         public NavigationIconButtonViewModel NavigationIcon => new NavigationIconButtonViewModel { Title = "設定", IconKey = "Cog" };
 
+        public Swatch[] Swatches { get; } = new SwatchesProvider().Swatches.ToArray();
+
+        public ConfigurationViewModel()
+        {
+            
+        }
+
         public void CreateDbFile()
         {
             Setup.CreateDatabaseFile();
+
         }
     }
 }
