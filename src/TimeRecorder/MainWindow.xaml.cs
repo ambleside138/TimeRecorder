28d5a5794384efe82290f50ca9c22f3930ddc42e
ambleside138 <ambleside.138@gmail.com>
2020-05-20 23:22:38 +0900

    feat: add notifyicon
    常駐化対応

--- src/TimeRecorder/MainWindow.xaml.cs
+++ src/TimeRecorder/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,44 @@ namespace TimeRecorder
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private NotifyIconProxy _NotifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var iconPath = new Uri("pack://application:,,,/TimeRecorder;component/clock_32.ico", UriKind.Absolute);
+            _NotifyIcon = new NotifyIconProxy(iconPath, "工数管理");
+            _NotifyIcon.DoubleClick += (_, __) => ShowWindow();
         }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // クローズ処理をキャンセルして、タスクバーの表示も消す
+            e.Cancel = true;
+            this.WindowState = System.Windows.WindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // ウィンドウを閉じる際に、タスクトレイのアイコンを削除する。
+            _NotifyIcon.Dispose();
+        }
+
+        private void ShowWindow()
+        {
+            // ウィンドウ表示&最前面に持ってくる
+            if (this.WindowState == System.Windows.WindowState.Minimized)
+                this.WindowState = System.Windows.WindowState.Normal;
+
+            this.Show();
+            this.Activate();
+            this.ShowInTaskbar = true;
+        }
+
+
     }
 }
---
+++ src/TimeRecorder/NotifyIconProxy.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeRecorder
+{
+    // 参考
+    // http://sourcechord.hatenablog.com/entry/2017/02/11/125649
+    // https://github.com/sourcechord/WPFSamples/blob/master/NotifyIconSample/NotifyIconSample/NotifyIconEx.cs
+
+    public enum ToolTipIconKind
+    {
+        None = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+    class NotifyIconProxy
+    {
+        private System.Windows.Forms.NotifyIcon _notify;
+
+        public string Text
+        {
+            get { return _notify.Text; }
+            set { _notify.Text = value; }
+        }
+
+        public bool Visible
+        {
+            get { return _notify.Visible; }
+            set { _notify.Visible = value; }
+        }
+
+        public Uri IconPath
+        {
+            set
+            {
+                if (value == null) { return; }
+                var iconStream = System.Windows.Application.GetResourceStream(value).Stream;
+                _notify.Icon = new System.Drawing.Icon(iconStream);
+            }
+        }
+
+        public System.Windows.Controls.ContextMenu ContextMenu { get; set; }
+
+        public ToolTipIconKind BalloonTipIcon
+        {
+            get { return (ToolTipIconKind)_notify.BalloonTipIcon; }
+            set { _notify.BalloonTipIcon = (System.Windows.Forms.ToolTipIcon)value; }
+        }
+
+        public string BalloonTipTitle
+        {
+            get { return _notify.BalloonTipTitle; }
+            set { _notify.BalloonTipTitle = value; }
+        }
+
+        public string BalloonTipText
+        {
+            get { return _notify.BalloonTipText; }
+            set { _notify.BalloonTipText = value; }
+        }
+
+        public void ShowBalloonTip(int timeout)
+        {
+            _notify.ShowBalloonTip(timeout);
+        }
+
+        public void ShowBalloonTip(int timeout, string tipTitle, string tipText, ToolTipIconKind tipIcon)
+        {
+            var icon = (System.Windows.Forms.ToolTipIcon)tipIcon;
+            _notify.ShowBalloonTip(timeout, tipTitle, tipText, icon);
+        }
+
+        public NotifyIconProxy()
+            : this(null) { }
+
+        public NotifyIconProxy(Uri iconPath)
+            : this(iconPath, null) { }
+
+        public NotifyIconProxy(Uri iconPath, string text)
+            : this(iconPath, text, null) { }
+
+        public NotifyIconProxy(Uri iconPath, string text, System.Windows.Controls.ContextMenu menu)
+        {
+            // 各種プロパティを初期化
+            _notify = new System.Windows.Forms.NotifyIcon();
+            IconPath = iconPath;
+            Text = text;
+            ContextMenu = menu;
+
+            // マウス右ボタンUpのタイミングで、ContextMenuの表示を行う
+            // ダミーの透明ウィンドウを表示し、このウィンドウのアクティブ状態を用いてContextMenuの表示/非表示を切り替える
+            _notify.MouseUp += (s, e) =>
+            {
+                if (e.Button != System.Windows.Forms.MouseButtons.Right) { return; }
+
+                var win = new System.Windows.Window()
+                {
+                    WindowStyle = System.Windows.WindowStyle.None,
+                    ShowInTaskbar = false,
+                    AllowsTransparency = true,
+                    Background = System.Windows.Media.Brushes.Transparent,
+                    Content = new System.Windows.Controls.Grid(),
+                    ContextMenu = ContextMenu
+                };
+
+                var isClosed = false;
+                win.Activated += (_, __) =>
+                {
+                    if (win.ContextMenu != null)
+                    {
+                        win.ContextMenu.IsOpen = true;
+                    }
+                };
+                win.Closing += (_, __) =>
+                {
+                    isClosed = true;
+                };
+
+                win.Deactivated += (_, __) =>
+                {
+                    if (win.ContextMenu != null)
+                    {
+                        win.ContextMenu.IsOpen = false;
+                    }
+                    if (!isClosed)
+                    {
+                        win.Close();
+                    }
+                };
+
+                // ダミーウィンドウ表示&アクティブ化をする。
+                // ⇒これがActivatedイベントで、ContextMenuが表示される
+                win.Show();
+                win.Activate();
+            };
+
+            _notify.Visible = true;
+        }
+
+        #region NotifyIconクラスの各種イベントをラップする
+        public event EventHandler BalloonTipClicked
+        {
+            add { _notify.BalloonTipClicked += value; }
+            remove { _notify.BalloonTipClicked -= value; }
+        }
+
+        public event EventHandler BalloonTipClosed
+        {
+            add { _notify.BalloonTipClosed += value; }
+            remove { _notify.BalloonTipClosed -= value; }
+        }
+
+        public event EventHandler BalloonTipShown
+        {
+            add { _notify.BalloonTipShown += value; }
+            remove { _notify.BalloonTipShown -= value; }
+        }
+
+        public event EventHandler Click
+        {
+            add { _notify.Click += value; }
+            remove { _notify.Click -= value; }
+        }
+
+        public event EventHandler Disposed
+        {
+            add { _notify.Disposed += value; }
+            remove { _notify.Disposed -= value; }
+        }
+
+        public event EventHandler DoubleClick
+        {
+            add { _notify.DoubleClick += value; }
+            remove { _notify.DoubleClick -= value; }
+        }
+        #endregion
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _notify.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+    }
+}
