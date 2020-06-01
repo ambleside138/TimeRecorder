a72d30a4b2fc035a596f6cf2a37fb4c8f015f4dc
ambleside138 <ambleside.138@gmail.com>
2020-06-01 22:12:24 +0900

    feat: 二重起動防止

--- build/ReleaseBuild.bat
+++ build/ReleaseBuild.bat
@@ -1,4 +1,4 @@
 rmdir /s /q Release
 mkdir Release
 
-dotnet publish "../src/TimeRecorder.sln" -c Release --self-contained  -r win10-x64 -o "Release"
+dotnet publish "../src/TimeRecorder.sln" -c Release --self-contained  -r win10-x64 -o "Release" > build.log
--- src/TimeRecorder/App.xaml.cs
+++ src/TimeRecorder/App.xaml.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using TimeRecorder.Host;
@@ -15,8 +18,22 @@ namespace TimeRecorder
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _Mutex = new Mutex(false, "TimeRecorder");
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (_Mutex.WaitOne(0, false) == false)
+            {
+                // 起動済みのウィンドウをアクティブにする
+                ShowPrevProcess();
+
+                // ここまで
+                _Mutex.Close();
+                _Mutex = null;
+                Shutdown();
+                return;
+            }
+
             DispatcherUnhandledException += App_DispatcherUnhandledException;
 
             base.OnStartup(e);
@@ -58,6 +75,45 @@ namespace TimeRecorder
 
             return ex.Message;
         }
+
+
+        #region 別プロセスのウィンドウを前面にだす
+        // タスクトレイに収まっている場合に未対応
+
+        public static bool ShowPrevProcess()
+        {
+            var thisProcess = Process.GetCurrentProcess();
+            var hProcesses = Process.GetProcessesByName(thisProcess.ProcessName);
+            var iThisProcessId = thisProcess.Id;
+
+            foreach (var hProcess in hProcesses)
+            {
+                if (hProcess.Id != iThisProcessId)
+                {
+                    ShowWindow(hProcess.MainWindowHandle, SW_NORMAL);
+                    SetForegroundWindow(hProcess.MainWindowHandle);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Auto)]
+        private static extern int ShowWindow(
+        System.IntPtr hWnd,
+        int nCmdShow
+    );
+
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Auto)]
+        private static extern bool SetForegroundWindow(
+            System.IntPtr hWnd
+        );
+
+
+        private const int SW_NORMAL = 1; 
+        #endregion
 
     }
 }
