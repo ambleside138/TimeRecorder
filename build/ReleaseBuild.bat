303616e7897b16831d47f906a1dbd4defc3cb993
ambleside138 <ambleside.138@gmail.com>
2021-01-26 23:15:14 +0900

    chore: trim corelib

--- build/ReleaseBuild.bat
+++ build/ReleaseBuild.bat
@@ -1,5 +1,5 @@
 rmdir /s /q Release
 mkdir Release
 
-dotnet publish "../src/TimeRecorder/TimeRecorder.csproj" -c Release -r win10-x64 -p:PublishSingleFile=true --self-contained true -o "Release" > build.log
+dotnet publish "../src/TimeRecorder/TimeRecorder.csproj" -c Release -r win10-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -o "Release" > build.log
 
