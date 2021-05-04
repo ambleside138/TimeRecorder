c0d232e63fd77c890807e3f5d19d9755b679d08e
ambleside138 <ambleside.138@gmail.com>
2021-05-04 12:24:55 +0900

    feat: 昼休憩時間の設定追加

--- src/TimeRecorder/Configurations/Items/ConfigItemBase.cs
+++ src/TimeRecorder/Configurations/Items/ConfigItemBase.cs
@@ -13,6 +13,7 @@ namespace TimeRecorder.Configurations.Items
         ScheduleTitleMap,
         WorkingHourImportApiUrl,
         ImportParam,
+        LunchTime
     };
 
     abstract class ConfigItemBase
---
+++ src/TimeRecorder/Configurations/Items/LunchTimeConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeRecorder.Configurations.Items
+{
+    /// <summary>
+    /// 昼休憩時間設定を表します
+    /// </summary>
+    class LunchTimeConfig : ConfigItemBase
+    {
+        public string StartHHmm { get; set; }
+
+        public string EndHHmm { get; set; }
+
+        internal override ConfigKey Key => ConfigKey.LunchTime;
+
+        
+    }
+}
