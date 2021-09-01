3bb2febcd635bed7c3a03d99d740730d130ce33f
ambleside138 <ambleside.138@gmail.com>
2021-09-02 08:22:24 +0900

    feat: use messagepipe

---
+++ src/TimeRecorder/Contents/Todo/TodoItemModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeRecorder.Domain.Domain.Todo;
+using TimeRecorder.Domain.UseCase.Todo;
+
+namespace TimeRecorder.Contents.Todo
+{
+    class TodoItemModel
+    {
+        private readonly TodoItem _DomainModel;
+
+        private readonly TodoUseCase _TodoUseCase;
+
+
+        public TodoItemModel(TodoItem todoItem)
+        {
+            _DomainModel = todoItem;
+            _TodoUseCase = new TodoUseCase(ContainerHelper.Resolver.Resolve<ITodoRepository>());
+        }
+
+        public async void UpdateAsync()
+        {
+
+        }
+
+        public async void DeleteAsync()
+        {
+        // メッセージ機構でTodoModelに通知したい
+        // https://github.com/Cysharp/MessagePipe
+        }
+    }
+}
